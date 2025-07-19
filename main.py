e990bf257c96f0fca0e04c1a54c85529a9dfd78f
Suren Khorenyan <surenkhorenyan@gmail.com>
2025-07-19 14:30:17 +0300

    create greet

--- main.py
+++ main.py
@@ -1,5 +1,39 @@
+import gradio as gr
+
+
+def handle_greet(
+    name: str = "",
+) -> str:
+    name = name.strip()
+    if not name:
+        name = "World"
+
+    return f"Hello, {name}!"
+
+
+with gr.Blocks() as app:
+    gr.Markdown("# Example Gradio App")
+
+    name_input = gr.Textbox(
+        label="Name",
+        value="",
+    )
+    btn = gr.Button(
+        "Greet",
+    )
+    greeting_output = gr.Textbox(
+        label="Greeting",
+        value="Hello",
+    )
+    btn.click(
+        fn=handle_greet,
+        inputs=[name_input],
+        outputs=[greeting_output],
+    )
+
+
 def main():
-    print("Hello from python-image-search!")
+    app.launch()
 
 
 if __name__ == "__main__":
