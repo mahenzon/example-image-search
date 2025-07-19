1d7e2eb42a16f21271e164b2eda626893b1e36da
Suren Khorenyan <surenkhorenyan@gmail.com>
2025-07-19 15:14:53 +0300

    update checkbox group

--- main.py
+++ main.py
@@ -1,34 +1,60 @@
+from typing import Any
+
 import gradio as gr
 
 
-def handle_greet(
+known_langs: tuple[str, ...] = (
+    "JS",
+    "Python",
+    "Go",
+    "PHP",
+    "Kotlin",
+)
+
+
+def handle_intro(
     name: str = "",
-) -> str:
+) -> tuple[str, dict[str, Any] | None]:
     name = name.strip()
     if not name:
         name = "World"
 
-    return f"Hello, {name}!"
+    greeting_out = f"Hello, {name}!"
+    found_langs = list(
+        {lang for lang in known_langs} & set(name.replace(",", " ").split())
+    )
+    cbg_out = gr.update(
+        label=f"New result includes {len(found_langs)} langs:",
+        value=found_langs,
+    )
+    return greeting_out, cbg_out
 
 
 with gr.Blocks() as app:
     gr.Markdown("# Example Gradio App")
 
-    name_input = gr.Textbox(
-        label="Name",
+    intro_input = gr.Textbox(
+        label="Introduce yourself",
         value="",
     )
     btn = gr.Button(
         "Greet",
+        variant="primary",
     )
     greeting_output = gr.Textbox(
         label="Greeting",
-        value="Hello",
+        value="",
+        interactive=False,
     )
+    cbg = gr.CheckboxGroup(
+        choices=[lang for lang in known_langs],
+        interactive=False,
+    )
+
     btn.click(
-        fn=handle_greet,
-        inputs=[name_input],
-        outputs=[greeting_output],
+        fn=handle_intro,
+        inputs=[intro_input],
+        outputs=[greeting_output, cbg],
     )
 
 
