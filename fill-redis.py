8bb89b5e2b37c0a10af1d1aa718dc50348a5a46f
Suren Khorenyan <surenkhorenyan@gmail.com>
2025-07-26 19:02:03 +0300

    create index

--- fill-redis.py
+++ fill-redis.py
@@ -1,8 +1,16 @@
 import logging
 from collections.abc import Sequence
 from dataclasses import asdict
+from typing import Final
 
 from redis import Redis
+from redis.exceptions import ResponseError
+from redis.commands.search.field import (
+    TextField,
+    NumericField,
+    TagField,
+)
+from redis.commands.search.index_definition import IndexDefinition, IndexType
 
 from config import (
     REDIS_PRODUCTS_DB,
@@ -16,6 +24,37 @@ from products_data import generate_products
 
 
 log = logging.getLogger(__name__)
+
+INDEX_SCHEMA: Final = [
+    TextField("id"),
+    TextField("name", sortable=True),
+    NumericField("price", sortable=True),
+    TextField("brand", sortable=True),
+    TagField("category", sortable=True),
+    NumericField("rating", sortable=True),
+]
+
+INDEX_DEFINITION = IndexDefinition(
+    prefix=[REDIS_PRODUCTS_KEY_PREFIX],
+    index_type=IndexType.HASH,
+)
+
+
+def create_index(
+    redis: Redis,
+):
+    log.info("Creating redis index %r", REDIS_PRODUCTS_INDEX)
+
+    try:
+        result = redis.ft(REDIS_PRODUCTS_INDEX).create_index(
+            INDEX_SCHEMA,
+            definition=INDEX_DEFINITION,
+        )
+    except ResponseError as e:
+        if "Index already exists" in e.args:
+            log.info("Index %r already exists", REDIS_PRODUCTS_INDEX)
+    else:
+        log.info("Index created: %s", result)
 
 
 def fill_database(
@@ -42,6 +81,8 @@ def main():
         db=REDIS_PRODUCTS_DB,
         decode_responses=True,
     )
+    create_index(redis)
+
     products = generate_products(count=1000)
     fill_database(
         redis=redis,
