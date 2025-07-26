bded68f886d44d534b2e0d6e58e8172cca1bc582
Suren Khorenyan <surenkhorenyan@gmail.com>
2025-07-26 18:36:33 +0300

    add config

---
+++ config.py
@@ -0,0 +1,9 @@
+LOG_FORMAT: str = (
+    "[%(asctime)s.%(msecs)03d] %(module)10s:%(lineno)-3d %(levelname)-7s - %(message)s"
+)
+
+DATE_FORMAT: str = "%Y-%m-%d %H:%M:%S"
+
+REDIS_PRODUCTS_INDEX = "products_index"
+REDIS_PRODUCTS_DB = 1
+REDIS_PRODUCTS_HASH_NAME = "products"
